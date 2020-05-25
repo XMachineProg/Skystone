f71242d0b26a67a06d0db472f04d2c8b550946b0
Antonio <antoniosouza.dev@gmail.com>
2020-05-25 08:34:51 -0300

    Adiciona comentarios guias, e remove funcao redundante

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/activations/MotorActivation.kt
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/activations/MotorActivation.kt
@@ -11,13 +11,11 @@ class MotorActivation : Activation{
     public lateinit var motor: DcMotor // Motor que vai ser ativado.
     public var forcaAtivada by Delegates.notNull<Double>() // Força que sera aplicada ao motor, quando ele for ativado.
     public var forcateste: Double = 3.0
-    fun setaForca(forca: Double) {
-        this.forcaAtivada = forca
-    }
-
     fun ativadorDoMotor(motor: DcMotor, forcaAtivada: Double) { // Recebe força do motor
         this.motor = motor
         this.forcaAtivada = forcaAtivada
+
+        // log
     }
 
     override fun setaAtivacao(ativado: Boolean) { // Envia as informaçoes da ativaçao para o motor.
@@ -27,18 +25,24 @@ class MotorActivation : Activation{
         } else {
             motor.power = 0.0
         }
+
+        // log
     }
 
     override fun retornaAtivacao(): Boolean { // Retorna se o motor esta ativo ou nao.
+
+        // log
         return motorAtivo
     }
 
     override fun ativa() { // Ativa motor
         setaAtivacao(true)
+        // log
     }
 
     override fun desativa() { // Desativa motor
         setaAtivacao(false)
+        // log
     }
 
 
