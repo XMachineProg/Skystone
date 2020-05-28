23538968a31e34596e7e8284dfbac30552ab9bb9
T0ny <62897985+4ntonioSouza@users.noreply.github.com>
2020-05-28 10:36:30 -0300

    simple fix

--- TeamCode/src/main/java/org/firstinspires/ftc/sevenxmachine/storeExecute/guardaMovimentos.kt
+++ TeamCode/src/main/java/org/firstinspires/ftc/sevenxmachine/storeExecute/guardaMovimentos.kt
@@ -1,7 +1,7 @@
 package org.firstinspires.ftc.sevenxmachine.storeExecute
 
 class guardaMovimentos {
-    var array = arrayOf<Array<Int>>() // Inicializa uma array bi-dimensional
+    private var array = arrayOf<Array<Int>>() // Inicializa uma array bi-dimensional
 
     fun zeraArray(): Array<Array<Int>> { // Preenche todos os valores da array para "0"
         for (i in 0..9) { // Percorre, definindo tamanho dela.
@@ -15,7 +15,7 @@ class guardaMovimentos {
         return array // Retorna a array, zerada
     }
 
-    fun preencheArray(m: Array<Array<Int>>, p1: Int, p2: Int, p3: Int, p4: Int) {
+    fun preencheArray(m: Array<Array<Int>>, p1: Int, p2: Int, p3: Int, p4: Int) { // p1 e p3 = motor, p2 e p4 = força.
         for (i in m.indices) { // Percorre a array via indices
             for (j in m[i].indices) {
                 if (m[i][j] == 0) { // Se a o 1° valor da linha é vazia, o resto também vai ser.
